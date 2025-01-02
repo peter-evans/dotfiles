8ad8051b5d069e3a828190370a76006f2c09907f
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2025-01-02 11:34:00 +0000

    Add install script and gitconfig

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
 My dotfiles
+
+## Install
+
+```bash
+git clone https://github.com/peter-evans/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Creates symlinks from the home directory to dotfiles in ~/dotfiles
+
+dotfiles_dir=~/dotfiles
+backup_dir=~/dotfiles_backup
+files="gitconfig" # space-separated list of files/folders to symlink in homedir
+
+# Backup existing dotfiles
+echo -n "Creating $backup_dir for backup of any existing dotfiles in ~ ..."
+mkdir -p $backup_dir
+echo "done"
+
+# Change to the dotfiles directory
+echo -n "Changing to the $dotfiles_dir directory ..."
+cd $dotfiles_dir
+echo "done"
+
+# Backup any existing dotfiles and then create symlinks
+for file in $files; do
+    if [ -L ~/.$file ]; then
+        echo "$file is a symlink; skipping backup"
+    else
+        echo -n "Moving $file from ~ to $backup_dir if it exists ..."
+        mv ~/.$file $backup_dir/
+        echo "done"
+    fi
+    echo -n "Creating symlink to $file in home directory ..."
+    ln -sf $dotfiles_dir/$file ~/.$file
+    echo "done"
+done
